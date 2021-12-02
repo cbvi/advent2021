114bf002072bbfd27d448d85e2c0fc462377d58a
Carlin Bingham <cb@viennan.net>
2021-12-03 01:57:07 +1300

    read types directly

--- src/day02.adb
+++ src/day02.adb
@@ -1,86 +1,48 @@
 with Ada.Text_IO;
-with Ada.Containers.Formal_Vectors;
+with Ada.IO_Exceptions;
 
 procedure day02 is
    
-   package IO renames Ada.Text_IO;
-   
    type Direction_Type is (Forward, Down, Up);
    
-   Oops : exception;
+   package IO renames Ada.Text_IO;
    
-   type Instruction is record
-      Direction : Direction_Type;
-      Distance  : Long_Long_Integer;
-   end record;
-   
-   package Integer_Vector is new Ada.Containers.Formal_Vectors
-     (Index_Type   => Natural,
-      Element_Type => Integer);
-   
-   package IV renames Integer_Vector;
-   
-   function Get_Instruction (Line : String) return Instruction
-   is
-      Ins : Direction_Type;
-      -- Dis : Long_Long_Integer;
-      Break : Integer := 0;
-   begin
-      for C of Line loop
-         Break := Break + 1;
-         if C = ' ' then
-            exit;
-         end if;
-      end loop;
-      
-      declare
-         Word : String := Line (Line'First .. Break - 1);
-         Number : String := Line (Break + 1 .. Line'Last);
-         Int : Long_Long_Integer;
-         Res : Instruction;
-      begin
-         Int := Long_Long_Integer'Value (Number);
-         
-         Ins := Direction_Type'Value (Word);
-         
-
-         
-         Res.Direction := Ins;
-         Res.Distance := Int;
-         return Res;
-      end;
-   end Get_Instruction;
+   package Long_IO      is new IO.Integer_IO     (Long_Long_Integer);
+   package Direction_IO is new IO.Enumeration_IO (Direction_Type);
    
    File : IO.File_Type;
-   Line : String (1..1024);
-   Last : Natural;
    
-   Horiz : Long_Long_Integer := 0;
-   Depth : Long_Long_Integer := 0;
-   My_Ins : Instruction;
-   Aim : Long_Long_Integer := 0;
+   Horiz  : Long_Long_Integer := 0;
+   Depth  : Long_Long_Integer := 0;
+   Aim    : Long_Long_Integer := 0;
+   
+   Direction : Direction_Type;
+   Distance  : Long_Long_Integer;
 begin
    IO.Open (File, IO.In_File, "day2.txt", "WCEM=8");
    
    while true loop
       begin
-         IO.Get_Line (File, Line, Last);
-         My_Ins := Get_Instruction (Line (Line'First .. Last));
-         case My_Ins.Direction is
+         Direction_IO.Get (File, Direction);
+         Long_IO.Get (File, Distance);
+         case Direction is
             when Up =>
-               --Depth := Depth - My_Ins.Distance;
-               Aim := Aim - My_Ins.Distance;
+               --Depth := Depth - Distance;
+               Aim := Aim - Distance;
             when Down =>
-               --Depth := Depth + My_Ins.Distance;
-               Aim := Aim + My_Ins.Distance;
+               --Depth := Depth + Distance;
+               Aim := Aim + Distance;
             when Forward =>
-               Horiz := Horiz + My_Ins.Distance;
-               Depth := Depth + (Aim * My_Ins.Distance);
+               Horiz := Horiz + Distance;
+               Depth := Depth + (Aim * Distance);
          end case;
       exception
-         when others => exit;
+         when Ada.IO_Exceptions.End_Error =>
+            exit;
       end;
    end loop;
+   
+   IO.Close (File);
    
    IO.Put_Line (Long_Long_Integer'Image (Horiz));
    IO.Put_Line (Long_Long_Integer'Image (Depth));
