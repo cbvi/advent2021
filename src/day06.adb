9dbcca92a383975d12739c001225a96736e6f697
Carlin Bingham <cb@viennan.net>
2021-12-07 02:32:01 +1300

    day 6, part 2, without more fish than will fit in ram...
    assuming part 2 would add more attributes to the fish was a mistake

--- src/day06.adb
+++ src/day06.adb
@@ -1,42 +1,38 @@
 with Ada.Text_IO;
 with Ada.Strings.Fixed;
-with Ada.Containers.Formal_Vectors;
 
-procedure day06 is
-   type Lanternfish is record
-      Timer : Integer;
-   end record;
+procedure day06 is  
    
-   package Lanternfish_Vector is new Ada.Containers.Formal_Vectors
-     (Index_Type => Natural,
-      Element_Type => Lanternfish);
+   type Fish_Count is mod 2**64;
+   type Fish_Cycle is range 0 .. 8;
+   type Fish_Map is array (Fish_Cycle) of Fish_Count;
    
-   package LV renames Lanternfish_Vector;
+   Spawn_Point : constant Fish_Cycle := 8;
+   Reset_Point : constant Fish_Cycle := 6;
+    
    package IO renames Ada.Text_IO;
    
    procedure Spawn_Lanternfish
-     (Lanternfishes : in out LV.Vector; Life : Integer := 8)
+     (Lanternfishes : in out Fish_Map; Life : Fish_Cycle := 8)
    is
-      Fish : Lanternfish;
    begin
-      Fish.Timer := Life;
-      LV.Append (Lanternfishes, Fish);
+      Lanternfishes (Life) := Lanternfishes (Life) + 1;
    end Spawn_Lanternfish;
    
    procedure Spawn_Initial_Lanternfish
-     (Line : String; Lanternfishes : in out LV.Vector)
+     (Line : String; Lanternfishes : in out Fish_Map)
    is
       Index : Natural;
       Last  : Natural := Line'First;
-      Life : Integer;
+      Life  : Fish_Cycle;
    begin
       loop
          Index := Ada.Strings.Fixed.Index (Line (Last .. Line'Last), ",");
          if Index /= 0 then
-            Life := Integer'Value (Line (Last .. Index - 1));
+            Life := Fish_Cycle'Value (Line (Last .. Index - 1));
             Spawn_Lanternfish (Lanternfishes, Life);
          else
-            Life := Integer'Value (Line (Last .. Line'Last));
+            Life := Fish_Cycle'Value (Line (Last .. Line'Last));
             Spawn_Lanternfish (Lanternfishes, Life);
             exit;
          end if;
@@ -44,24 +40,28 @@ procedure day06 is
       end loop;
    end Spawn_Initial_Lanternfish;
    
-   procedure Tick_Day (Lanternfishes : in out LV.Vector)
+   procedure Tick_Day (Lanternfishes : in out Fish_Map)
    is
-      Fish : Lanternfish;
+      New_Fish : constant Fish_Count := Lanternfishes (Fish_Cycle'First);
    begin
-      for I in LV.First_Index (Lanternfishes) .. LV.Last_Index (Lanternfishes)
-      loop
-         Fish := LV.Element (Lanternfishes, I);
-         if Fish.Timer = 0 then
-            Fish.Timer := 6;
-            Spawn_Lanternfish (Lanternfishes);
-         else
-            Fish.Timer := Fish.Timer - 1;
-         end if;
-         LV.Replace_Element (Lanternfishes, I, Fish);
+      for I in Lanternfishes'First + 1 .. Lanternfishes'Last loop
+         Lanternfishes (I - 1) := Lanternfishes (I);
       end loop;
+      Lanternfishes (Spawn_Point) := New_Fish;
+      Lanternfishes (Reset_Point) := Lanternfishes (Reset_Point) + New_Fish;
    end Tick_Day;
    
-   Lanternfishes : LV.Vector (524288);
+   function Total_Fish (Lanternfishes : Fish_Map) return Fish_Count
+   is
+      Total : Fish_Count := 0;
+   begin
+      for I in Lanternfishes'Range loop
+         Total := Total + Lanternfishes (I);
+      end loop;
+      return Total;
+   end Total_Fish;
+   
+   Lanternfishes : Fish_Map := (others => 0);
    File : IO.File_Type;
    Line : String (1 .. 1024);
    Last : Natural;
@@ -71,10 +71,10 @@ begin
    IO.Close (File);
    
    Spawn_Initial_Lanternfish (Line (Line'First .. Last), Lanternfishes);
-   
-   for I in 1 .. 80 loop
+
+   for I in 1 .. 256 loop
       Tick_Day (Lanternfishes);
    end loop;
    
-   IO.Put_Line (Integer'Image (Integer (LV.Length (Lanternfishes))));
+   IO.Put_Line (Fish_Count'Image (Total_Fish (Lanternfishes)));
 end day06;
