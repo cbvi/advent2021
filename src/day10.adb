e80e87375e19a2ba51cb3ed55d39c3cea185833f
Carlin Bingham <cb@viennan.net>
2021-12-10 20:22:03 +1300

    day 10, part 2
    more or less a complete rewrite

--- src/day10.adb
+++ src/day10.adb
@@ -9,13 +9,18 @@ procedure day10 is
      (Index_Type => Natural,
       Element_Type => Character);
    
+   package Score_Vector is new Ada.Containers.Formal_Vectors
+     (Index_Type => Natural,
+      Element_Type => Long_Long_Integer);
+   
+   package Sort_Score is new Score_Vector.Generic_Sorting;
+   
    package TV renames Token_Vector;
+   package SV renames Score_Vector;
    
-   Wtf : TV.Vector (4096);
-   
-   function Parse_Line (Line : String; Stack : in out TV.Vector)
-                        return Character
+   function Validate_Line (Line : String) return Boolean
    is
+      Toks : TV.Vector (4096);
       C : Character;
       D : Character;
    begin
@@ -23,62 +28,82 @@ procedure day10 is
          C := Line (I);
          case C is
             when '<' | '{' | '[' | '(' =>
-               TV.Append (Stack, C);
+               TV.Append (Toks, C);
             when '>' | '}' | ']' | ')' =>
-               D := TV.Last_Element (Stack);
-               TV.Delete_Last (Stack);
+               D := TV.Last_Element (Toks);
+               TV.Delete_Last (Toks);
                if D = '<' and then C /= '>' then
-                  TV.Append (Wtf, '<');
-                  return C;
+                  return False;
                elsif D = '{' and then C /= '}' then
-                  TV.Append (Wtf, '{');
-                  return C;
+                  return False;
                elsif D = '[' and then C /= ']' then
-                  TV.Append (Wtf, '[');
-                  return C;
+                  return False;
                elsif D = '(' and then C /= ')' then
-                  TV.Append (Wtf, '(');
-                  return C;
+                  return False;
                end if;
             when others => null;
          end case;
       end loop;
-      return '0';
-   end Parse_Line;
+      return True;
+   end Validate_Line;
    
-   Delims : TV.Vector (4096);
+   function Get_Score (Line : String) return Long_Long_Integer
+   is
+      Tale : Long_Long_Integer := 0;
+      Toks : TV.Vector (4096);
+   begin
+      declare
+         C : Character;
+      begin
+         for I in Line'First .. Line'Last loop
+            C := Line (I);
+            case C is
+            when '<' | '{' | '[' | '(' =>
+               TV.Append (Toks, C);
+            when '>' | '}' | ']' | ')' =>
+               TV.Delete_Last (Toks);
+            when others => null;
+            end case;
+         end loop;
+      end;
+      
+      TV.Reverse_Elements (Toks);
+      
+      for C of Toks loop
+         Tale := Tale * 5;
+         case C is
+            when '<' => Tale := Tale + 4;
+            when '{' => Tale := Tale + 3;
+            when '[' => Tale := Tale + 2;
+            when '(' => Tale := Tale + 1;
+            when others => null;
+         end case;
+      end loop;
+
+      return Tale;
+   end Get_Score;
+   
    File : IO.File_Type;
    Line : String (1 .. 1024);
    Last : Natural;
-   Tale : Natural := 0;
-   Illegal : TV.Vector (4096);
-   Char : Character;
+   Scores : SV.Vector (4096);
 begin
    IO.Open (File, IO.In_File, "day10.txt", "WCEM=8");
    loop
       begin
          IO.Get_Line (File, Line, Last);
-         Char := Parse_Line (Line (Line'First .. Last), Delims);
-         case Char is
-            when '>' | '}' | ']' | ')' =>
-               TV.Append (Illegal, Char);
-            when others =>
-               null;
-         end case;
+         if Validate_Line (Line (Line'First .. Last)) then
+            sv.Append (Scores, Get_Score (Line (Line'First .. Last)));
+         end if;
       exception
          when Ada.IO_Exceptions.End_Error => exit;
       end;
    end loop;
    
-   for C of Illegal loop
-      case C is
-         when '>' => Tale := Tale + 25137;
-         when '}' => Tale := Tale + 1197;
-         when ']' => Tale := Tale + 57;
-         when ')' => Tale := Tale + 3;
-         when others => null;
-      end case;
-   end loop;
+   Sort_Score.Sort (Scores);
    
-   IO.Put_Line (Natural'Image (Tale));
+   IO.Put_Line
+     (Long_Long_Integer'Image
+        (SV.Element
+             (Scores, SV.First_Index (Scores) + Sv.Last_Index (Scores) / 2)));
 end day10;
